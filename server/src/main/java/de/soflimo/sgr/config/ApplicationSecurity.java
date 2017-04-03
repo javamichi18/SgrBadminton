36b9effd3cd8e4bf808f3f2aac0b0db530c8f876
Michael Untermann <m.untermann@soflimo.de>
2017-04-03 09:10:22 +0200

    auth check rest services configurable

--- server/src/main/java/de/soflimo/sgr/config/ApplicationSecurity.java
+++ server/src/main/java/de/soflimo/sgr/config/ApplicationSecurity.java
@@ -1,8 +1,13 @@
 package de.soflimo.sgr.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,9 +32,15 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationSecurity.class);
 
+    @Value("${security.unprotected.resource.paths}")
+    private String unprotectedPaths;
+
 
     @Override
-    protected void configure (HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
+
+        String[] unprotectedPaths = getUnprotectedPaths();
+        log.info("UNPROTECTED: " + Arrays.asList(getUnprotectedPaths()));
 
         http
             .csrf().disable()
@@ -37,36 +48,36 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
         http
             .authorizeRequests()
-                //.antMatchers("/").access("hasRole('USER')")
-            .antMatchers("/css/**", "/js/**", "/fonts/**", "/pic/**").permitAll()
-            .antMatchers("/console/**").permitAll()
+            .antMatchers(getUnprotectedPaths()).permitAll()
             .anyRequest().authenticated()
-            .and()
 
-            .formLogin()
-            .loginPage("/login.html")
-            .loginProcessingUrl("/login")
-                //.defaultSuccessUrl("/sgrbadminton/index.html")
-            .permitAll()
             .and()
-
             .logout()
             .permitAll();
+
+        if (unprotectedPaths == null || !contains(unprotectedPaths, "rest")) {
+            http
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                //.defaultSuccessUrl("/sgrbadminton/index.html")
+                .permitAll();
+        }
     }
 
 
     @Override
-    protected void configure (
+    protected void configure(
         AuthenticationManagerBuilder auth) throws Exception {
 
         UserDetailsService userDetailsService = new UserDetailsService() {
             @Override
-            public UserDetails loadUserByUsername (String username)
+            public UserDetails loadUserByUsername(String username)
                 throws UsernameNotFoundException {
 
                 Spieler spieler = spielerRepository.findByEmail(username);
                 if (spieler != null) {
-                    log.info("user login: "+spieler);
+                    log.info("user login: " + spieler);
                     return spieler.mapUserDetails();
                 }
                 throw new UsernameNotFoundException("User '" + username + "' not found.");
@@ -76,5 +87,37 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+
+
+    private boolean contains(String[] unprotectedPaths, String fragment) {
+
+        if (unprotectedPaths == null || unprotectedPaths.length == 0)
+            return false;
+
+        for (String path : unprotectedPaths)
+            if (path.contains(fragment))
+                return true;
+
+        return false;
+    }
+
+
+    private String[] getUnprotectedPaths() {
+
+        List<String> paths = new ArrayList<>();
+        if (unprotectedPaths != null && unprotectedPaths.length() > 0) {
+            String[] split = unprotectedPaths.split(",");
+            for (String val : split)
+                paths.add(getPath(val));
+        } else
+            paths.add(getPath("css"));
+
+        return paths.toArray(new String[0]);
+    }
+
+
+    private String getPath(String p) {
+        return "/" + p + "/**";
     }
 }
